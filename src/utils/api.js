15c6c91fdeea4b12891b19dc77cf4a77f56fc673
Roy Lee <roylee@Roys-MacBook-Pro.local>
2020-03-07 19:01:11 -0800

    fixed heroku api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3000/rooms';
+const url = '/rooms';
+
+if (process.env.NODE_ENV === 'development') {
+  url = 'http://localhost:3000/rooms';
+}
 
 export const getRooms = (cb) => {
   axios.get(url)
